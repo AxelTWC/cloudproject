3fc247698aa3296de0768a74552d4aa820e38739
AxelTWC <30543699+AxelTWC@users.noreply.github.com>
2025-12-01 17:29:05 -0500

    Update route.ts

--- cloudproject/app/api/backup/create/route.ts
+++ cloudproject/app/api/backup/create/route.ts
@@ -9,14 +9,14 @@ export async function POST() {
     const command = `docker run --rm --network cloudproject_default \
       -e PGHOST=db \
       -e PGPORT=5432 \
-      -e PGUSER=${process.env.DB_USER} \
-      -e PGPASSWORD=${process.env.DB_PASSWORD} \
-      -e PGDATABASE=${process.env.DB_NAME} \
-      -e SPACES_KEY=${process.env.SPACES_KEY} \
-      -e SPACES_SECRET=${process.env.SPACES_SECRET} \
-      -e SPACES_BUCKET=${process.env.SPACES_BUCKET} \
-      -e SPACES_ENDPOINT=${process.env.SPACES_ENDPOINT} \
-      -e SPACES_REGION=${process.env.SPACES_REGION} \
+      -e PGUSER=postgres \
+      -e PGPASSWORD=123 \
+      -e PGDATABASE=cloudproject \
+      -e SPACES_KEY=DO00ABEDTKZQZTQA39AN \
+      -e SPACES_SECRET=JFySfaRlPqZHJu4C+fgx0uS5MSjMlhpv3qKobOJVriw \
+      -e SPACES_BUCKET=cloudproject \
+      -e SPACES_ENDPOINT=https://cloudproject.tor1.digitaloceanspaces.com \
+      -e SPACES_REGION=tor1 \
       -e PREFIX=backups/ \
       local/pg-backup:latest`;
 
